920f41eb44a4b933a8e0e2550c0ab2bcfb284ee6
Dzulfikar <kardzulfi1805@gmail.com>
2021-08-09 13:29:54 +0700

    Add Products
    Created Products
    - controller
    - migration
    - seeder

---
+++ app/Models/Products.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Products extends Model
+{
+    protected $fillable = [
+        'product_name',
+        'product_stock',
+        'product_price'
+    ];
+
+}
---
+++ database/migrations/2021_08_09_050244_create_product_list_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_list', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('product_name')->unique();
+            $table->integer('product_stock');
+            $table->bigInteger('product_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_list');
+    }
+}
