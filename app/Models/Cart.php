b0792f4a5bfcc807536137800c06e721f4ecdfa9
Dzulfikar <kardzulfi1805@gmail.com>
2021-08-11 23:12:35 +0700

    Create Cart Model

---
+++ app/Models/Cart.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cart';
+    protected $primayKey = 'cart_id';
+
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'quantity'
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'cart_id';
+    }
+}
---
+++ database/migrations/2021_08_10_091933_create_cart_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id('cart_id');
+            $table->foreignId('transaction_id')->references('transaction_id')->on('transactions');
+            $table->foreignId('product_id')->references('product_id')->on('products');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
