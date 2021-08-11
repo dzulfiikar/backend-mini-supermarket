ca45f7ea91192b997b9eb25c53ee56d6042419b4
Dzulfikar <kardzulfi1805@gmail.com>
2021-08-11 23:09:25 +0700

    Update Handler
    Update handling ModelNotFound

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -37,5 +38,18 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        if($e instanceof ModelNotFoundException){
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Resource not found',
+                'data' => []
+            ], 404);
+        }
+
+        return parent::render($request, $e);
     }
 }
